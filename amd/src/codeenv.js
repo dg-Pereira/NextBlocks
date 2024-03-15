24f340927d98148d03ae61c2460db25cc1402864
dg-Pereira <dg.pereira@campus.fct.unl.pt>
2024-03-15 23:59:26 +0000

    .

--- amd/src/codeenv.js
+++ amd/src/codeenv.js
@@ -758,41 +758,41 @@ javascript.javascriptGenerator.forBlock.text_multiline_input = function(block, g
     return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-class CustomCategory extends Blockly.ToolboxCategory {
-    /**
-     * Constructor for a custom category.
-     * @override
-     */
-    constructor(categoryDef, toolbox, optParent) {
-        super(categoryDef, toolbox, optParent);
-    }
+//class CustomCategory extends Blockly.ToolboxCategory {
+//    /**
+//     * Constructor for a custom category.
+//     * @override
+//     */
+//    constructor(categoryDef, toolbox, optParent) {
+//        super(categoryDef, toolbox, optParent);
+//    }
+//
+//    /** @override */
+//    addColourBorder_(colour) {
+//        this.rowDiv_.style.backgroundColor = colour;
+//    }
+//
+//    /** @override */
+//    setSelected(isSelected) {
+//        // We do not store the label span on the category, so use getElementsByClassName.
+//        var labelDom = this.rowDiv_.getElementsByClassName('blocklyTreeLabel')[0];
+//        if (isSelected) {
+//            // Change the background color of the div to white.
+//            this.rowDiv_.style.backgroundColor = 'white';
+//            // Set the colour of the text to the colour of the category.
+//            labelDom.style.color = this.colour_;
+//            this.iconDom_.style.color = this.colour_;
+//        } else {
+//            // Set the background back to the original colour.
+//            this.rowDiv_.style.backgroundColor = this.colour_;
+//            // Set the text back to white.
+//            labelDom.style.color = 'white';
+//            this.iconDom_.style.color = 'white';
+//        }
+//        // This is used for accessibility purposes.
+//        Blockly.utils.aria.setState(/** @type {!Element} */ (this.htmlDiv_),
+//            Blockly.utils.aria.State.SELECTED, isSelected);
+//    }
+//}
 
-    /** @override */
-    addColourBorder_(colour) {
-        this.rowDiv_.style.backgroundColor = colour;
-    }
-
-    /** @override */
-    setSelected(isSelected) {
-        // We do not store the label span on the category, so use getElementsByClassName.
-        var labelDom = this.rowDiv_.getElementsByClassName('blocklyTreeLabel')[0];
-        if (isSelected) {
-            // Change the background color of the div to white.
-            this.rowDiv_.style.backgroundColor = 'white';
-            // Set the colour of the text to the colour of the category.
-            labelDom.style.color = this.colour_;
-            this.iconDom_.style.color = this.colour_;
-        } else {
-            // Set the background back to the original colour.
-            this.rowDiv_.style.backgroundColor = this.colour_;
-            // Set the text back to white.
-            labelDom.style.color = 'white';
-            this.iconDom_.style.color = 'white';
-        }
-        // This is used for accessibility purposes.
-        Blockly.utils.aria.setState(/** @type {!Element} */ (this.htmlDiv_),
-            Blockly.utils.aria.State.SELECTED, isSelected);
-    }
-}
-
-Blockly.registry.register(Blockly.registry.Type.TOOLBOX_ITEM, Blockly.ToolboxCategory.registrationName, CustomCategory, true);
+//Blockly.registry.register(Blockly.registry.Type.TOOLBOX_ITEM, Blockly.ToolboxCategory.registrationName, CustomCategory, true);
