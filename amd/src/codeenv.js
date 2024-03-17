513cc87897a93ab86da135b5e334f1a05d76ce18
dg-Pereira <dg.pereira@campus.fct.unl.pt>
2024-03-17 23:33:15 +0000

    Added many new blocks to the toolbox

--- amd/src/codeenv.js
+++ amd/src/codeenv.js
@@ -36,12 +36,58 @@ let toolbox = {
                 },
                 {
                     'kind': 'block',
+                    'type': 'logic_negate',
+                },
+                {
+                    'kind': 'block',
                     'type': 'logic_operation',
                 },
                 {
                     'kind': 'block',
                     'type': 'logic_boolean',
                 },
+                {
+                    'kind': 'block',
+                    'type': 'logic_null',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'logic_ternary',
+                }
+            ],
+        },
+        {
+            'kind': 'category',
+            'name': 'Loops',
+            'colour': '5ba580',
+            "cssConfig": {
+                'icon': 'customIcon fa-solid fa-sync',
+            },
+            'contents': [
+                {
+                    'kind': 'block',
+                    'type': 'controls_repeat_ext',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'controls_whileUntil',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'controls_for',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'controls_forEach',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'controls_repeat',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'controls_flow_statements',
+                }
             ],
         },
         {
@@ -60,6 +106,50 @@ let toolbox = {
                     'kind': 'block',
                     'type': 'math_arithmetic',
                 },
+                {
+                    'kind': 'block',
+                    'type': 'math_single',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_trig',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_constant',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_number_property',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_round',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_on_list',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_modulo',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_constrain',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_random_int',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_random_float',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'math_atan2',
+                },
             ],
         },
         {
@@ -76,8 +166,114 @@ let toolbox = {
                 },
                 {
                     'kind': 'block',
+                    'type': 'text_multiline',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_join',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_append',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_length',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_isEmpty',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_indexOf',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_charAt',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_getSubstring',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_changeCase',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_trim',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_count',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_replace',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'text_reverse',
+                },
+                {
+                    'kind': 'block',
                     'type': 'text_print',
                 },
+            ],
+        },
+        {
+            'kind': 'category',
+            'name': 'Lists',
+            'colour': '5b80a5',
+            "cssConfig": {
+                'icon': 'customIcon fa-solid fa-list',
+            },
+            'contents': [
+                {
+                    'kind': 'block',
+                    'type': 'lists_create_with',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_repeat',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_length',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_isEmpty',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_indexOf',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_getIndex',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_setIndex',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_getSublist',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_split',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_sort',
+                },
+                {
+                    'kind': 'block',
+                    'type': 'lists_reverse',
+                }
             ],
         },
         {
